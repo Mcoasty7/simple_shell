1eeb8692b3f7b637fa6b26bc2e6184f69603cc59
vickouma77 <vickprestone20@gmail.com>
2022-11-16 12:18:54 -0800

    correction

--- exits.c
+++ exits.c
@@ -1,23 +1,74 @@
 #include "shell.h"
-/**
- * execute_prog - Executes binary or builtin
- * @args: Argument buffer
- * @line: Line buffer
- * @env: Environment
- * @flow: Helps decide what code to run
- * Return: 1 if success
- */
-int execute_prog(char **args, char *line, char **env, int flow)
-{
-	int status;
 
-	if (args[0] == NULL)
-		return (1);
-	if (flow == 1)
+/**
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
 	{
-		if (check_for_builtins(args, line, env) == 1)
-			return (1);
+		dest[i] = src[i];
+		i++;
 	}
-	status = launch_prog(args);
-	return (status);
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
 }
